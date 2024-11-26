23909b04840dc529dcc3f834f5a866a0f497323e
berkutivan <58652635+berkutivan@users.noreply.github.com>
2024-11-26 23:55:06 +0300

    Update drawing_functions.py

--- drawing_functions.py
+++ drawing_functions.py
@@ -1,16 +1,22 @@
 import  numpy as np
 import cv2
-from functions import  correct_point
+from functions import  Corrcet
 
 
-def draw_cluster(image, clusters):
+def draw_cluster(image, clusters, filt = None):
     for cluster in clusters:
-        cluster.draw(image)
+        if filt == None:
+            #print("OK")
+            cluster.draw(image)
+            continue
+        if filt(cluster):
+            #print("OK")
+            cluster.draw(image)
 
 def draw_center(image, points):
     correct = []
     for point in points:
-        if correct_point(image, point):
+        if Corrcet(image).point(point):
             correct.append(point)
     correct = np.array(correct)
 
@@ -18,9 +24,7 @@ def draw_center(image, points):
         center = np.mean(correct, 0)
         cv2.circle(image, (int(center[0]), int(center[1])), 25, (0, 0, 255), 2)
 
-def draw_point(image, point):
-
-    if correct_point(image, point):
-        cv2.circle(image, (int(point[0]),int(point[1])), radius=5, color=(0, 0, 0), thickness=-1)
-
-    return  image
+def draw_point(image, points):
+    for point  in points:
+        if Corrcet(image).point(point):
+            cv2.circle(image, (int(point[0]),int(point[1])), radius=5, color=(0, 0, 0), thickness=-1)
