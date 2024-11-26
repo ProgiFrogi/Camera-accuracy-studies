eac840ca5a285613f11782e013091e1c939cc913
berkutivan <58652635+berkutivan@users.noreply.github.com>
2024-11-26 23:54:37 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,29 +1,10 @@
 import cv2
-import numpy as np
 from random import randint
-from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.cluster import HDBSCAN, DBSCAN
 from mpl_toolkits import mplot3d
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
-
-from hyper_params import *
-
-def convert_image(image):
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    height, width, _ = image_rgb.shape
-    pixels = image_rgb.reshape(-1, 3)
-
-    k = 3
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    kmeans.fit(pixels)
-
-    clustered_pixels = kmeans.cluster_centers_[kmeans.labels_].astype(np.uint8)
-    clustered_frame = clustered_pixels.reshape(height, width, 3)
-    return clustered_frame
 
 def points_to_param(points):
     x1, y1, x2, y2 = points
@@ -71,110 +52,49 @@ def contour_cords(image, line, method = "calc"):
         return True
     return np.array(point)
 
-'''
-def concatenate_line(image, lines):
-    a_arr, b_arr, c_arr = [], [], []
-    points = []
-    for line in lines:
-        a, b, c, l = points_to_param(line[0])
-        a_arr.append(a)
-        b_arr.append(b)
-        c_arr.append(c)
-        line = Line_cluster([line])
-        point = 
-        points.append(contour_cords(image, line))
-    a_arr = np.array(a_arr) / np.max(np.abs(a_arr))
-    b_arr = np.array(b_arr) / np.max(np.abs(b_arr))
-    c_arr = np.array(c_arr) / np.max(np.abs(c_arr))
+def one_line_metric(line1, line2):
+    x_start_1, x_end_1 = line1.get_section_x()
+    x_start_2, x_end_2 = line2.get_section_x()
 
-    S = Metric(image)
-    points = 2 * np.array(points) / S.perimetr
-    dbscan = DBSCAN(eps=eps_dbscan_for_clusters/8, min_samples=1, metric=S.count)
-    labels = dbscan.fit_predict(points)
-    #plt.scatter(points[:,0], points[:,1], c= labels)
-    #plt.show()
+    x_start = min(x_start_1, x_start_2)
+    x_end = max(x_end_1, x_end_2)
 
-    concatenate_lines = {}
+    y1, y2 = line1.y(x_start), line2.y(x_start)
+    dy1 = abs(y1 - y2)
 
-    for i in range(len(lines)):
-        ind = labels[i]
-        if ind in concatenate_lines :
-            concatenate_lines[ind].append(lines[i])
-        else:
-            concatenate_lines[ind] = [lines[i]]
+    y1, y2 = line1.y(x_end), line2.y(x_end)
+    dy2 = abs(y1 - y2)
 
-    return concatenate_lines
-'''
+    l1 = abs(line1.l * (x_end - x_start) / (x_end_1 - x_start_1))
+    l2 = abs(line2.l * (x_end - x_start) / (x_end_2 - x_start_2))
 
-def concatenate_line(image, lines):
-    points = []
-    correct_lines = []
-    for line in lines:
-        line_p = Line_cluster([line])
-        if contour_cords(image, line_p, method= 'check'):
-            point = contour_cords(image, line_p)
-            points.append(point)
-            correct_lines.append(line)
+    return  (dy1 + dy2) / (l1 + l2)
+def line_metric(line1, line2):
+    a1, b1, c1, l1 = points_to_param(line1)
+    a2, b2, c2, l2 = points_to_param(line2)
+    x_start_1, x_end_1 = line1[0], line1[2]
+    x_start_2, x_end_2 = line2[0], line2[2]
+    x_start = min(x_start_1, x_start_2)
+    x_end = max(x_end_1, x_end_2)
+    y1 = -(c1 + a1 * x_start) / b1
+    y2 = -(c2 + a2 * x_start) / b2
+    dy1 = abs(y1 - y2)
+    y1 = -(c1 + a1 * x_end) / b1
+    y2 = -(c2 + a2 * x_end) / b2
+    dy2 = abs(y1 - y2)
+    l1 = abs(l1 * (x_end - x_start) / (x_end_1 - x_start_1))
+    l2 = abs(l2 * (x_end - x_start) / (x_end_2 - x_start_2))
+    return (dy1 + dy2) / (l1 + l2)
 
-    #print(len(lines), np.shape(points))
-    S = Metric(image)
-    points = np.array(points) / S.perimetr
-    dbscan = DBSCAN(eps=eps_dbscan_for_small_lines, min_samples=1, metric=S.count)
-    labels = dbscan.fit_predict(points)
-    #plt.scatter(points[:,0], points[:,1], c= labels)
-    #plt.show()
+def warp_perspective_to_top_view(image, src_points, dst_points, output_size):
+    # Вычисляем матрицу преобразования перспективы
+    src = np.array(src_points, dtype=np.float32)
+    dst = np.array(dst_points, dtype=np.float32)
+    matrix = cv2.getPerspectiveTransform(src, dst)
 
-    concatenate_lines = {}
-
-    for i in range(len(correct_lines)):
-        ind = labels[i]
-        if ind in concatenate_lines :
-            concatenate_lines[ind].append(correct_lines[i])
-        else:
-            concatenate_lines[ind] = [correct_lines[i]]
-
-    return concatenate_lines
-
-def merge_in_clusters(image, lines):
-    global eps_dbscan_for_clusters
-    global min_cluster_len
-
-    Line_clusters = []
-    points = []
-
-    for label in lines:
-        Lines = Line_cluster(lines[label])
-
-        if Lines.l > min_cluster_len  and contour_cords(image, Lines, method= 'check'):
-            Line_clusters.append(Lines)
-            points.append(contour_cords(image, Lines))
-
-    S = Metric(image)
-    points = np.array(points) / S.perimetr
-    dbscan = DBSCAN(eps=eps_dbscan_for_clusters, min_samples=1, metric= S.count)
-    labels = dbscan.fit_predict(points)
-    #plt.scatter(points[:,0], points[:,1], c= labels)
-    #plt.show()
-
-    concatenate_clusters = {}
-
-    for i in range(len(Line_clusters)):
-        ind = labels[i]
-        if ind in concatenate_clusters:
-            concatenate_clusters[ind].append(Line_clusters[i])
-        else:
-            concatenate_clusters[ind] = [Line_clusters[i]]
-
-    Clusters = [ Cluster(lines_dict) for lines_dict in concatenate_clusters.values()]
-
-    return Clusters
-
-def correct(image, line):
-    if contour_cords(image, line, method= 'check') == False:
-        return False
-    else:
-        return True
-
+    # Применяем преобразование к изображению
+    warped_image = cv2.warpPerspective(image, matrix, output_size)
+    return warped_image
 def intersection(line1, line2):
     a1, b1, c1 = line1
     a2, b2, c2 = line2
@@ -196,140 +116,141 @@ def intersections_clusters(cluster1, cluster2):
             points.append(point)
     return points
 
-def correct_point(image, point):
-    height, width, _ = image.shape
-    x, y = point
-    if 0 < x < width and 0 < y < height:
-        return True
-    else:
-        return  False
-
-def find_lines(rgb_image):
-    gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
-    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-    edges = cv2.Canny(blurred_image, 50, 150)
-    lines = cv2.HoughLinesP(edges,
-                            rho=rho,
-                            theta=theta,
-                            threshold=threshold,
-                            minLineLength=minLineLength,
-                            maxLineGap=maxLineGap)
-
-    return lines
-
-def sort_lines_by_length(lines):
-    if lines is None:
-        return []
-
-    # Вычисляем длины линий и сохраняем их вместе с координатами
-    line_lengths = []
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)  # Длина линии
-        line_lengths.append((length, line))
-
-    # Сортируем по длине в порядке убывания
-    sorted_lines = sorted(line_lengths, key=lambda x: x[0], reverse=True)
-
-    # Извлекаем отсортированные линии
-    sorted_lines = [line for length, line in sorted_lines]
-
-    return sorted_lines
-
-'''def is_double_line(cluster):
-    S = []
-    lines = cluster.get_lines()
-    l = len(lines)
-    for i in range(1, l):
-        for j in range(i):
-            S.append(my_metric(lines[i], lines[j]))
-    if 20 > max(S) > 9:
-        return False
-    return True'''
-
-class Line_cluster():
+class Line():
 
     def __init__(self, lines):
         self.lines = lines
-        self.a, self.b, self.c, self.l = self.mean_line_param()
+        self.a, self.b, self.c, self.segment_len = self.mean_line_param()
+        self.l = self.get_len()
+    def get_len(self):
+        x1, x2 = self.get_section_x()
+        y1, y2 = self.y(x1), self.y(x2)
+        S = ((x2 -x1)**2 + (y2 - y1)**2)**0.5
+        if S == np.inf:
+            y1, y2 = self.get_section_y()
+            x1, x2 = self.x(y1), self.x(y2)
+            S = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-    def get_section(self):
+        return S
+
+    def get_section_x(self):
         x_start = min([line[0][0] for line in self.lines])
         x_end = max([line[0][2] for line in self.lines])
         return x_start, x_end
 
+    def get_section_y(self):
+        y_start = min([line[0][1] for line in self.lines])
+        y_end = max([line[0][3] for line in self.lines])
+        return y_start, y_end
+
+    def y(self, x):
+        return -(self.c + self.a*x)/self.b
+    def x(self, y):
+        return -(self.c + self.b * y) / self.a
+
     def mean_line_param(self):
-        a_lines, b_lines, c_lines, l_lines = [], [], [], 0
+        a_lines, b_lines, c_lines, l  = [], [], [], 0
 
         for line in self.lines:
             a, b, c, l = points_to_param(line[0])
             a_lines.append(a)
             b_lines.append(b)
             c_lines.append(c)
-            l_lines += l
+            l += l
 
         b_lines, a_lines = np.array(b_lines), np.array(a_lines)
-        b, a = np.mean(b_lines), np.mean(a_lines)
+        c, b, a = np.mean(c_lines), np.mean(b_lines), np.mean(a_lines)
 
-        return a, b, c, l_lines
+        return a, b, c, l
 
-    def print(self,image, color):
-        for line in self.lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(image, (x1, y1), (x2, y2), color, 2)
+    def print(self,image, color, type = "all", d = 2):
+        if type == "all":
+            for line in self.lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(image, (x1, y1), (x2, y2), color, d)
+        if type == "one":
+            if abs(self.b) > 0.00001:
+                x1, x2 = self.get_section_x()
+                y1, y2 = self.y(x1), self.y(x2)
+                cv2.line(image, (x1, int(y1)), (x2, int(y2)), color, d)
+            else:
+                y1, y2 = self.get_section_y()
+                x1, x2 = self.x(y1), self.x(y2)
+                cv2.line(image, ( int(x1),y1), (int(x2), y2), color, d)
 
 class Cluster():
-    def __init__(self, cluster_lines):
-        self.clusters_params, self.l =  self.get_param(cluster_lines)
-        self.lines = self.get_lines(cluster_lines)
-
-    def get_param(self, clusters_lines):
+    def __init__(self, lines):
+        self.clusters_params, self.l =  self.get_param(lines)
+        self.lines = self.get_lines(lines)
+    def segment_len(self):
+        l = 0
+        for line in self.lines:
+            l += line.segment_len
+        return l
+    def get_param(self, lines):
         params = []
         l_cluster = 0
-        for cluster in clusters_lines:
-            a,b,c, l = cluster.a, cluster.b, cluster.c, cluster.l
+        for line in lines:
+            a,b,c, l = line.a, line.b, line.c, line.l
             params.append([a,b,c])
             l_cluster += l
         return params, l_cluster
 
-    def get_lines(self, cluster_lines):
+    def get_lines(self, lines):
         lines_array = []
-        for line in cluster_lines:
+        for line in lines:
             lines_array.append(line)
         return lines_array
-
-    def draw(self, image):
-        color = (randint(10, 255), randint(10, 255), randint(10, 255))
-        for lines in self.lines:
-            lines.print(image, color= color)
-
-    def max_norm(self, image):
-        S = Metric(image).line_metric
-        n  = len(self.lines)
-        l = 0
-        for i in range(1,n):
+    def max_norm(self):
+        S = []
+        l = len(self.lines)
+        for i in range(1, l):
             for j in range(i):
-                l = max(l, S(self.lines[i], self.lines[j]))
-        return l
+                S.append(one_line_metric(self.lines[i], self.lines[j]))
+        return max(S)
+
+    def draw(self, image, color = None):
+        if color == None:
+            color = (randint(10, 255), randint(10, 255), randint(10, 255))
+        for lines in self.lines:
+            lines.print(image, color= color, type = 'one', d = 4)
 
     def get_lines_params(self):
         return self.clusters_params
 
+    def __gt__(self, other):
+        if self.l > other.l:
+            return True
+        return False
+
 class Metric():
     def __init__(self, image):
-        height, width, _ = image.shape
-        self.perimetr = 2*(height + width)
+        self.height, self.width, _ = image.shape
+        self.perimetr = 2*(self.height + self.width)
     def count(self, x,y):
         dx = abs(x[0] - y[0]) % self.perimetr
         dy = abs(x[1] - y[1]) % self.perimetr
         return (dx ** 2 + dy ** 2) ** 0.5
-    def line_metric(self, line1, line2):
-        x_start_1, x_end_1 = line1.get_section()
-        x_start_2, x_end_2 = line2.get_section()
-        x_start = min(x_end_1, x_end_2)
-        x_end = max(x_start_1, x_start_2)
-        a1, b1, c1 = line1.a, line1.b, line1.c
-        a2, b2, c2 = line2.a, line2.b, line2.c
-        return (abs(-(c1 + a1*x_start)/b1 + (c2 + a2*x_start)/b2) + abs(-(c1 + a1*x_end)/b1 + (c2 + a2*x_end)/b2))/2
+    def cluster_metric(self, x,y):
+        dx = abs(x[0] - y[0]) % self.perimetr
+        dy = abs(x[1] - y[1]) % self.perimetr
+        S = (dx ** 2 + dy ** 2) ** 0.5
+        return S/x[2] + S/y[2]
 
+class Corrcet():
+    def __init__(self, image):
+        self.image = image
 
+    def point(self, point):
+        height, width, _ = self.image.shape
+        x, y = point
+        if 0 < x < width and 0 < y < height:
+            return True
+        else:
+            return False
+    #возможно следующий метод не требуется
+    def line(self, line):
+        if contour_cords(self.image, line, method='check') == False:
+            return False
+        else:
+            return True
