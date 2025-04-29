4be2f7944a70a7cf7cfcb4ebcc5e135044dbec33
IgorShishkin12 <qwertyshishkin@yandex.ru>
2025-04-30 02:11:35 +0300

    slight improvements

--- utils/find_ellipse/__init__.py
+++ utils/find_ellipse/__init__.py
@@ -1,4 +1,4 @@
 from .build import build
 from .find_ellipse import FindEllipseRHT
-from .high_quality_ellipse_detection.python_wrapper import *
-__all__ = ["FindEllipseRHT",]
+from .high_quality_ellipse_detection.python_wrapper import find_ellipses_fast
+__all__ = ["FindEllipseRHT","find_ellipses_fast"]
